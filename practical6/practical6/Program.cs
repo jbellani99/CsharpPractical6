dc8cabc2e0387e363fab3bf3fcc743c0bdb6061a
Jayesh bellani <54457951+jbellani99@users.noreply.github.com>
2023-03-17 17:24:36 +0530

    completed practical

---
+++ practical6/practical6/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+public delegate void Notify();
+
+public class ProcessBussinessLogic {
+
+    public int flag = 0;    
+    public event Notify ProcessCompleted;
+
+    /// <summary>
+    /// it's Start the process and call OnprocessCompleted
+    /// <return>
+    /// void 
+    /// </return>
+    /// </summary>
+
+    public void StartProcess() {
+
+        Console.WriteLine("Process Started..");
+        OnprocessCompeleted();
+            
+    
+    }
+
+    /// <summary>
+    /// It's call ProcessCompleted function
+    /// <return>
+    /// void
+    /// </return>
+    /// </summary>
+    protected virtual void OnprocessCompeleted() {
+        
+            ProcessCompleted?.Invoke();
+           
+
+    }
+
+
+}
+
+
+public class call { 
+
+            public static void Main(string[] args) { 
+    
+                ProcessBussinessLogic bl = new ProcessBussinessLogic(); 
+                    bl.flag = 0;
+                    bl.ProcessCompleted += ProcessCompleted;
+        bl.StartProcess();  
+
+       
+
+    
+    }
+
+    /// <summary>
+    /// It's acknowledge the event is done
+    /// <return>
+    /// void
+    /// </return>
+    /// </summary>
+    static void ProcessCompleted()
+    {
+
+        Console.WriteLine("Method Invoked..");
+
+
+    }
+
+} 
